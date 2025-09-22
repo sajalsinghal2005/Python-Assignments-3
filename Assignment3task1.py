5f7877c0e28ce9cd468810f25f2cee76994c0203
sajalsinghal2005 <sajalsinghal62650@gmail.com>
2025-09-22 23:27:40 +0530

    Add files via upload

---
+++ Assignment3task1.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n-1)
+result = factorial(5)
+print("factorial of 5 is :",result)
---
+++ Assignment3task2.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number:"))
+import math
+print("Square root :" , math.sqrt(n))
+print("Logarithm:", math.log(n))
+print("Sine:", math.sin(n))
